c30f3d01bd3ab85b2002f31441bec6c76b7e9b13
Denys Budkov <denisydisney@icloud.com>
2025-04-09 00:31:46 +0200

    Initial commit

--- pin-up.py
+++ pin-up.py
@@ -2,22 +2,22 @@ import streamlit as st
 import datetime
 import io
 
-st.title("📅 Генератор .ics")
+st.title("📅 Generator .ics")
 
 # Ввод данных пользователем
-summary = st.text_input("Название события")
-description = st.text_area("Описание")
-location = st.text_input("Локация")
-event_url = st.text_input("Ссылка на событие")
-start_date = st.date_input("Дата начала")
-start_time = st.time_input("Время начала", value=datetime.time(23, 59))
-end_date = st.date_input("Дата окончания")
-end_time = st.time_input("Время окончания", value=datetime.time(23, 59))
-alarm_minutes = st.number_input("За сколько минут напомнить", min_value=0, step=5, value=60)
+summary = st.text_input("Event name")
+description = st.text_area("Description")
+location = st.text_input("Location")
+event_url = st.text_input("Event Link")
+start_date = st.date_input("Start date")
+start_time = st.time_input("Start time", value=datetime.time(23, 59))
+end_date = st.date_input("End date")
+end_time = st.time_input("End time", value=datetime.time(23, 59))
+alarm_minutes = st.number_input("How many minutes in advance to remind?", min_value=0, step=5, value=60)
 
 # Проверка корректности введенных дат и времени
 if datetime.datetime.combine(end_date, end_time) <= datetime.datetime.combine(start_date, start_time):
-    st.error("❌ Время окончания должно быть позже времени начала.")
+    st.error("❌ The end time must be later than the start time.")
 else:
     # Функция для генерации содержимого .ics файла
     def generate_ics_content():
@@ -47,7 +47,7 @@ END:VCALENDAR"""
         return ics_content
 
     # Кнопка для скачивания .ics файла
-    if st.button("Создать и скачать .ics файл"):
+    if st.button("Create and download .ics file"):
         ics_content = generate_ics_content()
         # Создание буфера в памяти и запись содержимого .ics
         buf = io.BytesIO()
@@ -56,7 +56,7 @@ END:VCALENDAR"""
 
         # Предоставление кнопки для скачивания файла
         st.download_button(
-            label="📥 Скачать event.ics",
+            label="📥 Download event.ics",
             data=buf,
             file_name="event.ics",
             mime="text/calendar"
